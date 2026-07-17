8b5b3dcff3a6102ca0570c98fdfcb2c9e3caae7e
Oliver Ellis <ellis.oliver@gmail.com>
2026-07-17 14:31:29 +0200

    Fixed duplicated blocks

--- home/oliver/shell.nix
+++ home/oliver/shell.nix
@@ -1,33 +1,4 @@
-{
-  pkgs,
-  ...
-}:
-
-{
-  programs.zsh = {
-    enable = true;
-
-    autosuggestion.enable = true;
-
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ll = "ls -lah";
-      gs = "git status";
-      rebuild = "sudo nixos-rebuild switch --flake .#desktop";
-    };
-  };
-
-  home.packages = with pkgs; [
-    eza
-    bat
-    ripgrep
-    fd
-  ];
-}{
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 
 {
   programs.zsh = {
