e9084cabac8be8ee9d5313667e214e1ee2b8a454
Oliver Ellis <ellis.oliver@gmail.com>
2026-07-17 15:22:56 +0200

    Updated Kitty configuration

--- home/oliver/terminal.nix
+++ home/oliver/terminal.nix
@@ -9,10 +9,35 @@
 
     settings = {
       confirm_os_window_close = 0;
-
       font_size = 12;
-
       enable_audio_bell = false;
+
+      # Appearance
+      background_opacity = "0.90";
+      background_blur = 20;
+
+      # Padding around terminal content
+      window_padding_width = 8;
+
+      # Cursor
+      cursor_shape = "beam";
+      cursor_blink_interval = 0;
+
+      # Large scrollback history
+      scrollback_lines = 100000;
+
+      # Tabs
+      tab_bar_edge = "top";
+      tab_bar_style = "powerline";
+
+      # Open links with Ctrl+Shift+Click
+      open_url_with = "default";
+
+      # Disable close confirmation
+      confirm_os_window_close = 0;
+
+      # Window behavior
+      remember_window_size = true;
     };
   };
 }
